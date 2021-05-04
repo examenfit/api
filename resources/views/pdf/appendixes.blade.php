6b2ede3bde716141dd1096f02b9b4503de33f4d1
Giel <stekelenburg@gmail.com>
2021-05-04 12:59:48 +0200

    Surpresses header when no appendixes available

--- resources/views/pdf/appendixes.blade.php
+++ resources/views/pdf/appendixes.blade.php
@@ -1,7 +1,11 @@
-<div class=appendixes>  
-  @include('pdf.appendixes-header')
-  @foreach($appendixes as $appendix)
-    @include('pdf.appendix', $appendix)
-  @endforeach
-  @include('pdf.appendixes-footer')
-</div>
+@if (empty($appendixes))
+  <!-- geen uitwerkbijlagen -->
+@else
+  <div class=appendixes>  
+    @include('pdf.appendixes-header')
+    @foreach($appendixes as $appendix)
+      @include('pdf.appendix', $appendix)
+    @endforeach
+    @include('pdf.appendixes-footer')
+  </div>
+@endif
