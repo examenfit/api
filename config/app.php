3177257e6888cf6f034fc298468b4fbbfabef6cb
Giel <stekelenburg@gmail.com>
2021-05-04 13:23:23 +0200

    Use internal ip

--- config/app.php
+++ config/app.php
@@ -58,7 +58,7 @@ return [
 
     'dashboard_url' => env('DASHBOARD_URL', null),
 
-    'examenfit_scripts_url' => env('EXAMENFIT_SCRIPTS_URL', '52.59.238.35'),
+    'examenfit_scripts_url' => env('EXAMENFIT_SCRIPTS_URL', '10.0.1.15'),
 
     /*
     |--------------------------------------------------------------------------
