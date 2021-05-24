ed6878b19ba43a998c96b5b9f49ef13e7cab339d
Giel <stekelenburg@gmail.com>
2021-05-24 16:24:21 +0200

    Fixes activation link

--- resources/views/mail/registration.blade.php
+++ resources/views/mail/registration.blade.php
@@ -3,4 +3,9 @@
   Beste {{ $registration->first_name }} {{ $registration->last_name }},<br>
   Bedankt voor je aanmelding!
 <p>
-  <a href="#">{{ $registration->activation_code }}</a>
+  Klik op onderstaande link om je proefaccount te activeren.
+<p>
+  <!-- fixme -->
+  <a href="https://staging-app.examenfit.nl/activate/{{ $registration->activation_code }}">
+    https://staging-app.examenfit.nl/activate/{{ $registration->activation_code }}
+  </a>
