1fd86ae4f7f72d2b487dc6c99227db92236926ed
Giel <stekelenburg@gmail.com>
2021-07-15 09:30:32 +0200

    Logging

--- app/Http/Controllers/Admin/TeacherDocumentController.php
+++ app/Http/Controllers/Admin/TeacherDocumentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Models\Exam;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Artisan;
 
 class TeacherDocumentController extends Controller
@@ -12,12 +13,15 @@ class TeacherDocumentController extends Controller
     public function index(Exam $exam)
     {
         try {
-            Artisan::call('ef:questioncorrection', ['exam' => $exam->id]);
+            Artisa::call('ef:questioncorrection', ['exam' => $exam->id]);
+            Log::info(Artisan::output());
 
             $path = storage_path("app/public/question-correction/{$exam->hash_id}.docx");
 
             if (file_exists($path)) {
                 return response()->download($path);
+            } else {
+                return response("document unavailable ($path)", 500);
             }
         } catch (\Exception $error) {
             return response($error->getMessage(), 500);
