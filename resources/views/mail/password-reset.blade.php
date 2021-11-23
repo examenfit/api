23722a397862d433717c789848d8dbd045148628
Sander Tiekstra <sandertiekstra@gmail.com>
2021-11-23 10:57:06 +0100

    Aanpassen tekst Password reset e-mail

--- resources/views/mail/password-reset.blade.php
+++ resources/views/mail/password-reset.blade.php
@@ -1,8 +1,15 @@
 <!doctype html>
-<p>
-  Beste {{ $user->first_name }} {{ $user->last_name }},<br>
-  Klik op onderstaande link om je wachtwoord opnieuw in te stellen.
-<p>
-  <a href="{{ $link }}">Wachtwoord opnieuw instellen</a>
-<p>
-  Heb je geen wachtwoord reset aangevraagd? Dan is je wachtwoord ongewijzigd en kan je nog steeds inloggen.
+
+<p>Beste {{ $user->first_name }} {{ $user->last_name }},</p>
+
+<p>Klik op deze link om je wachtwoord opnieuw in te stellen:</p>
+
+<p><a href="{{ $link }}">Wachtwoord opnieuw instellen</a><p>
+
+<p>Heb je geen verzoek gedaan om je wachtwoord te wijzigen? Dan kun je deze email negeren. Je wachtwoord is ongewijzigd en je kan nog steeds inloggen.</p>
+
+<p>Heb je problemen met inloggen? Mail ons je probleem op <a href="mailto:info@examenfit.nl">info@examenfit.nl</a>.</p>
+
+<p>Met vriendelijke groet,</p>
+
+<p>Team ExamenFit</p>
