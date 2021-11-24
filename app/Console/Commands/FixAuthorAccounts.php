f06cb5079498949ad0ed93d231c665c8f610cb1f
Giel <stekelenburg@gmail.com>
2021-11-24 14:01:19 +0100

    Author fix

---
+++ app/Console/Commands/FixAuthorAccounts.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+use App\Models\License;
+use App\Models\Seat;
+use App\Models\User;
+use App\Models\Privilege;
+use App\Models\Stream;
+use App\Models\Group;
+
+class FixAuthorAccounts extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'ef:fix:author-accounts';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Fix privileges';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+      $this->fix1();
+    }
+
+
+    function link($link, $first_name, $last_name, $role, $group)
+    {
+        $user = User::create([
+          'email' => Str::random(10).'@examenfit.nl',
+          'password' => '',
+          'role' => $role,
+          'first_name' => $first_name,
+          'last_name' => $last_name,
+          'link' => $link
+        ]);
+
+        $seat = Seat::create([
+          'license_id' => 1,
+          'user_id' => $user->id,
+          'role' => $user->role,
+          'first_name' => "Test $role",
+        ]);
+        $seat->groups()->sync([ $group->id ]);
+        $this->info("{$seat->user->email}");
+
+        if ($role === 'docent') {
+          $priv = Privilege::create([
+            'actor_seat_id' => $seat->id,
+            'action' => 'groepen beheren',
+            'object_type' => 'group',
+            'object_id' => $group->id,
+            'begin' => '2001-01-01',
+            'end' => '2111-01-01'
+          ]);
+          $this->info("{$priv->action} {$priv->object_type} {$priv->object_id}");
+        }
+
+        $streams = Stream::all();
+        $action = $role === 'docent' ? 'onbeperkt opgavensets samenstellen' : 'oefensets uitvoeren';
+        foreach($streams as $stream) {
+          $priv = Privilege::create([
+            'actor_seat_id' => $seat->id,
+            'action' => $action,
+            'object_type' => 'stream',
+            'object_id' => $stream->id,
+            'begin' => '2001-01-01',
+            'end' => '2111-01-01'
+          ]);
+          $this->info("{$priv->action} {$priv->object_type} {$priv->object_id}");
+        }
+    }
+
+    function fix1()
+    {
+      $license = License::find(1); // ExamenFit company license
+      $authors = User::query()
+        ->whereNull('link')
+        ->where('role', 'author')
+        ->get();
+      foreach($authors as $author) {
+        $author->link = Str::random(20);
+        $author->save();
+        $this->info("{$author->link} {$author->email}");
+        $group = Group::create([
+          'license_id' => 1,
+          'name' => $author->email . ' groep',
+          'is_active' => 1
+        ]);
+        $this->link($author->link, 'Do', 'Cent, van der', 'docent', $group);
+        $this->link($author->link, 'Leo', 'Eerling', 'leerling', $group);
+      }
+    }
+}
