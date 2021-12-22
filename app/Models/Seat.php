d39bc89bd2e769f160b3eea76e628291a45a5ee4
Giel <stekelenburg@gmail.com>
2021-12-22 11:40:17 +0100

    fillable

--- app/Models/Seat.php
+++ app/Models/Seat.php
@@ -13,6 +13,9 @@ class Seat extends Model
     public $fillable = [
         'license_id',
         'user_id',
+        'first_name',
+        'last_name',
+        'email',
         'role'
     ];
 
