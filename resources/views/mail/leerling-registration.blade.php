eed4e98dad1bd3b9374f244f23b881b570a443b6
Giel <stekelenburg@gmail.com>
2022-02-22 09:29:51 +0100

    teksten

--- resources/views/mail/leerling-registration.blade.php
+++ resources/views/mail/leerling-registration.blade.php
@@ -3,17 +3,22 @@
   Beste {{ $registration->first_name }} {{ $registration->last_name }},
 </p>
 
-<p>Je hebt je aangemeld voor een leerlinglicentie. Hiermee kun je:</p>
-<ul>
-  <li>Examenstof oefenen</li>
-  <li>...</li>
-</ul>
+<p>Leuk dat je ExamenFit gaat gerbuiken!</p>
 
 <p>
-  Klik op deze link om je proeflicentie te activeren.<br>
+  Klik op deze link om je licentie te activeren.<br>
   <!-- fixme -->
   <a href="{{ $link }}">{{ $link }}</a>
 </p>
+
+<p>
+  Met je licentie kun je:
+</p>
+<ul>
+  <li>Examenvragen oefenen bij alle examenonderwerpen</li>
+  <li>Examenvragen oefenen bij de hoofdstukken van je lesboek</li>
+  <li>Zien aan welke onderwerpen je nog moet wekren</li>
+</ul>
 
 <p>Veel succes!</p>
 
