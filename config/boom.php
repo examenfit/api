bb03c8462e58e01bca73388bd3d538bc2ba90ffa
Giel <stekelenburg@gmail.com>
2024-07-13 00:47:37 +0200

    +licenses-2024-2025

--- config/boom.php
+++ config/boom.php
@@ -2,10 +2,6 @@
 
 return [
 
-    'oidc_provider' => env('BOOM_OIDC_PROVIDER'),
-    'oidc_client_id' => env('BOOM_OIDC_CLIENT_ID'),
-    'oidc_client_secret' => env('BOOM_OIDC_CLIENT_SECRET'),
-
     'oidc' => [
         'provider' => env('BOOM_OIDC_PROVIDER'),
         'client' => [
@@ -16,28 +12,30 @@ return [
 
     'licenses' => [
 
-        '9789464421149' => [ 'nask1-vmbo', 'nask2-vmbo', ],
-        '9789464421187' => [ 'nask1-vmbo', 'nask2-vmbo', ],
-
-        '9789464421125' => [ 'natuurkunde-havo', 'natuurkunde-vwo', ],
-        '9789464421163' => [ 'natuurkunde-havo', 'natuurkunde-vwo', ],
-
-        '9789464421132' => [ 'scheikunde-havo', 'scheikunde-vwo', ],
-        '9789464421170' => [ 'scheikunde-havo', 'scheikunde-vwo', ],
+        '9789464421125' => [ 'until' => '2024-08-01', 'natuurkunde-havo', 'natuurkunde-vwo', ],
+        '9789464421132' => [ 'until' => '2024-08-01', 'scheikunde-havo', 'scheikunde-vwo', ],
+        '9789464421149' => [ 'until' => '2024-08-01', 'nask1-vmbo', 'nask2-vmbo', ],
+        '9789464421163' => [ 'until' => '2024-08-01', 'natuurkunde-havo', 'natuurkunde-vwo', ],
+        '9789464421170' => [ 'until' => '2024-08-01', 'scheikunde-havo', 'scheikunde-vwo', ],
+        '9789464421187' => [ 'until' => '2024-08-01', 'nask1-vmbo', 'nask2-vmbo', ],
 
         '9789493113183' => [
-		'docent',
-        	'nask1-vmbo', 'nask2-vmbo',
-        	'natuurkunde-havo', 'natuurkunde-vwo',
-        	'scheikunde-havo', 'scheikunde-vwo',
-        ],
-
-        // test
-        '9789492862815' => [
+            'role' => 'docent',
+            'until' => '2024-08-01',
+            'nask1-vmbo',
+            'nask2-vmbo',
+            'natuurkunde-havo',
+            'natuurkunde-vwo',
             'scheikunde-havo',
             'scheikunde-vwo',
-            'docent',
         ],
+
+// test
+//        '9789492862815' => [
+//            'role' => 'docent',
+//            'until' => '2025-08-01',
+//            'scheikunde-havo',
+//        ],
 
     ],
 
