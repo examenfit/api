40d9561689ad4de51c2b15b2e5a17b2fd3371fe4
Giel <stekelenburg@gmail.com>
2024-07-15 15:36:06 +0200

    +fix-until

--- config/boom.php
+++ config/boom.php
@@ -12,16 +12,16 @@ return [
 
     'licenses' => [
 
-        '9789464421125' => [ 'until' => '2024-08-01', 'natuurkunde-havo', 'natuurkunde-vwo', ],
-        '9789464421132' => [ 'until' => '2024-08-01', 'scheikunde-havo', 'scheikunde-vwo', ],
-        '9789464421149' => [ 'until' => '2024-08-01', 'nask1-vmbo', 'nask2-vmbo', ],
-        '9789464421163' => [ 'until' => '2024-08-01', 'natuurkunde-havo', 'natuurkunde-vwo', ],
-        '9789464421170' => [ 'until' => '2024-08-01', 'scheikunde-havo', 'scheikunde-vwo', ],
-        '9789464421187' => [ 'until' => '2024-08-01', 'nask1-vmbo', 'nask2-vmbo', ],
+        '9789464421125' => [ 'until' => '2025-08-01', 'natuurkunde-havo', 'natuurkunde-vwo', ],
+        '9789464421132' => [ 'until' => '2025-08-01', 'scheikunde-havo', 'scheikunde-vwo', ],
+        '9789464421149' => [ 'until' => '2025-08-01', 'nask1-vmbo', 'nask2-vmbo', ],
+        '9789464421163' => [ 'until' => '2025-08-01', 'natuurkunde-havo', 'natuurkunde-vwo', ],
+        '9789464421170' => [ 'until' => '2025-08-01', 'scheikunde-havo', 'scheikunde-vwo', ],
+        '9789464421187' => [ 'until' => '2025-08-01', 'nask1-vmbo', 'nask2-vmbo', ],
 
         '9789493113183' => [
             'role' => 'docent',
-            'until' => '2024-08-01',
+            'until' => '2025-08-01',
             'nask1-vmbo',
             'nask2-vmbo',
             'natuurkunde-havo',
